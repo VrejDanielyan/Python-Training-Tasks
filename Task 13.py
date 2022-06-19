21bd4ccdd2e2c66040fea2c95a81a635f03b618f
Armine Hasratyan <56727925+ArmineHasratyan@users.noreply.github.com>
2022-06-19 23:02:35 +0400

    Update Task 13.py

--- Task 13.py
+++ Task 13.py
@@ -1,4 +1,4 @@
-# One line anonymous function to return given number's cube and print
+# One line anonymous function to return given number's cube and print - all program should be in one line - given number is 5
 
 cube = lambda x:print(x**3)
 cube(5)
