b2cd2e454cd70085d83cc306185657c8bd8cf62f
HMovsesyan <hayarpi@instigatemobile.com>
2022-08-11 00:16:41 +0900

    Adding tasks 6-7

---
+++ Task_6.py
@@ -0,0 +1,13 @@
+Python code to print triangle with fixed size
+*****
+****
+***
+**
+*
+
+
+for row in range(5):
+    for col in range(row+1):
+        print("*", end="")
+    print()
+
---
+++ Task_7.py
@@ -0,0 +1,13 @@
+# Python code to print triangle with fixed size
+# *****
+# ****
+# ***
+# **
+# *
+
+for row in range(5):
+    for col in range(5-row):
+        print("*", end="")
+    print()
+
+
